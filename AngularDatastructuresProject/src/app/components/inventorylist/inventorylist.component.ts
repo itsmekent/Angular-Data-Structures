e3f059430910860597e006c58a592669db1608a3
itsmekent <nmendoza2383@gmail.com>
2024-10-10 11:26:58 +0800

    feat: add inventorylist component

---
+++ AngularDatastructuresProject/src/app/components/inventorylist/inventorylist.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventorylist',
+  templateUrl: './inventorylist.component.html',
+  styleUrl: './inventorylist.component.css'
+})
+export class InventorylistComponent {
+  inventoryList: string[] = [
+    'Game Disc',
+    'Gaming Mouse',
+    'Gaming Console',
+    'Gaming Headset',
+    'Controller'
+  ];
+  inventoryName: string = '';
+
+  addInventories() {
+    this.inventoryList.push(this.inventoryName);
+  }
+}
