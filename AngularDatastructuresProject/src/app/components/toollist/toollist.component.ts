49c65dc429c0f9efa66df99b06a141366b31af2a
itsmekent <nmendoza2383@gmail.com>
2024-10-10 11:11:26 +0800

    feat: add ToolList  component

---
+++ AngularDatastructuresProject/src/app/components/toollist/toollist.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toollist',
+  templateUrl: './toollist.component.html',
+  styleUrl: './toollist.component.css'
+})
+export class ToollistComponent {
+  toolList: string[] = [
+    'Hammer', 
+    'Saw', 
+    'Utility Knife', 
+    'Pliers', 
+    'Pry Bars'
+  ];
+ 
+  toolName: string = '';
+
+  addTools() {
+    this.toolList.push(this.toolName);
+  }
+}
