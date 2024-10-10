700f2ce8d9c43f24efea90c8d822742cb56c8f82
itsmekent <nmendoza2383@gmail.com>
2024-10-10 12:46:59 +0800

    feat: add exerciselist component

---
+++ AngularDatastructuresProject/src/app/components/exerciselist/exerciselist.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exerciselist',
+  templateUrl: './exerciselist.component.html',
+  styleUrl: './exerciselist.component.css'
+})
+export class ExerciselistComponent {
+  exerciseList: string[] = [
+    'Jogging',
+    'Cycling',
+    'Swimming',
+    'Jump Rope',
+    'Push-Ups'
+  ];
+  exerciseName: string = '';
+
+  addExercises() {
+    this.exerciseList.push(this.exerciseName);
+  }
+
+}
