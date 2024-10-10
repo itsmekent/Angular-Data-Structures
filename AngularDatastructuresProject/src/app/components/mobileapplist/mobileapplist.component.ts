73497a62df8641737d27496456e535f2d93bb47f
itsmekent <nmendoza2383@gmail.com>
2024-10-10 11:39:31 +0800

    feat: add mobileapplist component

---
+++ AngularDatastructuresProject/src/app/components/mobileapplist/mobileapplist.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobileapplist',
+  templateUrl: './mobileapplist.component.html',
+  styleUrl: './mobileapplist.component.css'
+})
+export class MobileapplistComponent {
+  mobileAppList: string[] = [
+    'Facebook',
+    'Messenger',
+    'Instagram',
+    'TikTok',
+    'GCash',
+    'YouTube'
+  ];
+  mobileAppName: string = '';
+
+  addMobileApps() {
+    this.mobileAppList.push(this.mobileAppName);
+  }
+}
